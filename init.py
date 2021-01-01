072adad18199788a23a2085652ef31d00e4a4b53
Ilya <ilyanepsh@gmail.com>
2021-01-01 23:59:35 +0300

    commit

--- init.py
+++ init.py
@@ -4,30 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 import json
 
-
-jo = `{"inline_keyboard" : 
-    [
-        [
-            {"text": "10","callback_data": "10"}, 
-            {"text": "20","callback_data": "20"}
-        ],
-        [
-            {"text": "30","callback_data": "30"}, 
-            {"text": "40","callback_data": "40"}
-        ]
-    ]
-}`
-
 t = 0
-last_message_id = 0
+last_update_id = 0
 token = os.getenv('TOKEN')
 my_chat_id = str(os.getenv('MY_CHAT_ID'))
 url = "https://api.telegram.org/bot{}/".format(token)
 states = { my_chat_id : 0}
 
-def send_message(u, text):
-    j = ""
-    r = requests.get(url + 'sendMessage?chat_id={}&text={}&reply_markup={}'.format(u['id'], text, j))
+def send_message(id, text, reply_markup = ""):
+    r = requests.get(url + 'sendMessage?chat_id={}&text={}&reply_markup={}'.format(id, text,reply_markup))
     print(r.status_code)
     return 1
 
@@ -40,18 +25,27 @@ def get_last_update(request):
 def decryption_update_data(res): 
     if res['result']: 
         data = res['result'].pop()
-        msg = data['message']
-        msg_id = msg['message_id']
+        update_id = data['update_id']
         u = {}
-        u['name'] = msg['from']['username']
-        u['id'] = msg['from']['id'] 
-        if 'text' in msg:
-            u['data'] = msg['text']
+        if 'message' in data:
+            msg = data['message']
+            msg_id = msg['message_id']
+            u['name'] = msg['from']['username']
+            u['id'] = msg['from']['id'] 
+            if 'text' in msg:
+                u['data'] = msg['text']
 
-        if 'sticker' in msg:
-            u['data'] = msg['sticker']
+            if 'sticker' in msg:
+                u['data'] = msg['sticker']
 
-        return u, msg_id
+        if 'callback_query' in data:
+            call_back = data['callback_query']
+            u['data'] = call_back['data']
+            u['name'] = call_back['from']['username']
+            u['id'] = call_back['from']['id'] 
+
+        
+        return u, update_id 
     else: 
         return 0, 0
 
@@ -61,30 +55,34 @@ def check_state(id): #database for states
 def change_state(id, st): 
     states[str(id)] = st
 
+def send_menu(id):
+    send_message(id, "Menu", '{%22keyboard%22:[[{%22text%22:%22read%22}],[{%22text%22:%22my+score%22}]]}')
+
 def descypt_message(u):
     state = check_state(u['id'])
     print(state, u['data'])
     if state == 0 :
         if u['data'] == 'read' : 
-            send_message(u, "how much time")
+            reply_markup = '{%22inline_keyboard%22%20:%20%0A%20%20%20%20[%0A%20%20%20%20%20%20%20%20[%0A%20%20%20%20%20%20%20%20%20%20%20%20{%22text%22:%20%2210%22,%22callback_data%22:%20%2210%22},%20%0A%20%20%20%20%20%20%20%20%20%20%20%20{%22text%22:%20%2220%22,%22callback_data%22:%20%2220%22}%0A%20%20%20%20%20%20%20%20],%0A%20%20%20%20%20%20%20%20[%0A%20%20%20%20%20%20%20%20%20%20%20%20{%22text%22:%20%2230%22,%22callback_data%22:%20%2230%22},%20%0A%20%20%20%20%20%20%20%20%20%20%20%20{%22text%22:%20%2240%22,%22callback_data%22:%20%2240%22}%0A%20%20%20%20%20%20%20%20]%0A%20%20%20%20]%0A}'
+            send_message(u['id'], "how much time", reply_markup)
             change_state(u['id'], 10)
 
     if state == 10 : 
         if u['data'] != 'read' : # check it is time 
             print(u['data'])
-            send_message(u, "save your time {} min".format(u['data']))
+            send_message(u['id'], "save your time {} min".format(u['data']))
             change_state(u['id'], 0)
+            send_menu(u['id'])
 
         else:  
-            send_message(u, "something go wrong send another time") 
+            send_message(u['id'], "something go wrong send another time") 
     
 
 while 1 :
     update = get_last_update(url)
-    print(update)
-    u, msg_id = decryption_update_data(update)
-    if msg_id != last_message_id:
-        last_message_id = msg_id
+    u, update_id = decryption_update_data(update)
+    if update_id != last_update_id:
+        last_update_id = update_id
         descypt_message(u)
         
 
